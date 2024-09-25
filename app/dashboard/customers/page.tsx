330c45c6c90fed00d01bd23e162e6c37d2be71a2
mccartheney <mccartheney@hotmail.com>
2024-09-25 14:59:50 +0100

    chapter 5

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+const Page : FC = () => {
+    return(
+        <h1>customers page</h1>
+    )
+}
+
+export default Page
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react"
+
+const Page : FC =() => {
+    return (
+        <h1>
+            This is dasboard route
+        </h1>
+    )
+}
+
+export default Page
